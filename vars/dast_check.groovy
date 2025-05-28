c2ecc152b47790a2de7f8871b245ccac8dace8f2
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-05-29 02:48:17 +0530

    Create dast_check.groovy

---
+++ vars/dast_check.groovy
@@ -0,0 +1,11 @@
+def call(){
+   steps {
+                sh """
+                    zaproxy -cmd \
+                    -port 8090 \
+                    -quickurl http://54.237.233.167:8080/apidocs/ \
+                    -quickout ${WORKSPACE}/zap-report.html \
+                    -quickprogress
+                    """
+            }
+}
