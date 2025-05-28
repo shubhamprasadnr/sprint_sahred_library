102705e256deb1131da5c76eaaa9d827b33be37a
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-05-29 03:01:51 +0530

    Update dast_check.groovy

--- vars/dast_check.groovy
+++ vars/dast_check.groovy
@@ -1,8 +1,19 @@
-def call(){
+// def call(){
+//                 sh """
+//                     zaproxy -cmd \
+//                     -port 8090 \
+//                     -quickurl http://54.237.233.167:8080/apidocs/ \
+//                     -quickout ${WORKSPACE}/zap-report.html \
+//                     -quickprogress
+//                     """
+// }
+
+
+def call(String targeturl){
                 sh """
                     zaproxy -cmd \
                     -port 8090 \
-                    -quickurl http://54.237.233.167:8080/apidocs/ \
+                    -quickurl ${targeturl} \
                     -quickout ${WORKSPACE}/zap-report.html \
                     -quickprogress
                     """
