9fecad2bb6b23edc79537a5a33f93c6d29c574e0
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-05-30 05:54:03 +0530

    Create py_sonarscan.groovy

---
+++ vars/py_sonarscan.groovy
@@ -0,0 +1,17 @@
+def call() {
+    steps {
+        script {
+            withSonarQubeEnv('SonarQube') {
+                withCredentials([string(credentialsId: 'attendance_cred', variable: 'SONAR_TOKEN')]) {
+                    def scannerHome = tool 'sonarqube'
+                    sh """
+                        ${scannerHome}/bin/sonar-scanner \
+                        -Dsonar.projectKey=${env.SONAR_PROJECT_KEY} \
+                        -Dsonar.sources=. \
+                        -Dsonar.login=$SONAR_TOKEN
+                    """
+                }
+            }
+        }
+    }
+}
