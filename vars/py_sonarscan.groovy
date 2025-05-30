13ee604c38c11e3dc8e4fca568cd5961e3f0b821
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-05-30 06:02:47 +0530

    Update py_sonarscan.groovy

--- vars/py_sonarscan.groovy
+++ vars/py_sonarscan.groovy
@@ -1,17 +1,15 @@
+// vars/static_code.groovy
 def call() {
-    steps {
-        script {
-            withSonarQubeEnv('SonarQube') {
-                withCredentials([string(credentialsId: 'attendance_cred', variable: 'SONAR_TOKEN')]) {
-                    def scannerHome = tool 'sonarqube'
-                    sh """
-                        ${scannerHome}/bin/sonar-scanner \
-                        -Dsonar.projectKey=${env.SONAR_PROJECT_KEY} \
-                        -Dsonar.sources=. \
-                        -Dsonar.login=$SONAR_TOKEN
-                    """
-                }
-            }
+    withSonarQubeEnv('SonarQube') {
+        withCredentials([string(credentialsId: 'attendance_cred', variable: 'SONAR_TOKEN')]) {
+            def scannerHome = tool 'sonarqube'
+            sh """
+                ${scannerHome}/bin/sonar-scanner \
+                -Dsonar.projectKey=${env.SONAR_PROJECT_KEY} \
+                -Dsonar.sources=. \
+                -Dsonar.login=$SONAR_TOKEN
+            """
         }
     }
 }
+
