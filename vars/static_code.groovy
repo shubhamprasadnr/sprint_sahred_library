26f1324207ca530dc286de1ff3616a7b47307f56
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-05-30 05:46:09 +0530

    Update static_code.groovy

--- vars/static_code.groovy
+++ vars/static_code.groovy
@@ -1,8 +1,15 @@
-def call() {
-    withSonarQubeEnv('SonarQube') {
-        sh """
-            mvn clean verify sonar:sonar \
-            -Dsonar.projectKey=${env.SONAR_PROJECT_KEY}
-        """
+stage('SonarQube Scan') {
+    steps {
+        script {
+            withSonarQubeEnv('sonarqube') {
+                withCredentials([string(credentialsId: 'sonarcred', variable: 'SONARQUBE_TOKEN')]) {
+                    sh """
+                        mvn sonar:sonar \
+                        -Dsonar.projectKey=${env.SONAR_PROJECT_KEY} \
+                        -Dsonar.login=${SONARQUBE_TOKEN}
+                    """
+                }
+            }
+        }
     }
 }
