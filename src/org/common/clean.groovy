73fe4f88598769a58c7d2637a382d10c57996251
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-06-18 07:51:20 +0530

    Update clean.groovy

--- src/org/common/clean.groovy
+++ src/org/common/clean.groovy
@@ -1,1 +1,11 @@
+package org.common
 
+class CleanWS implements Serializable {
+    def steps
+    CleanWS(steps) { this.steps = steps }
+
+    def call() {
+        steps.echo "Cleaning workspace..."
+        steps.cleanWs()
+    }
+}
