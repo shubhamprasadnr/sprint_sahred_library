258e14ed2d522b090472303b9533a95df6db73e8
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-06-19 08:45:45 +0530

    Update CloneRepo.groovy

--- src/org/common/CloneRepo.groovy
+++ src/org/common/CloneRepo.groovy
@@ -5,7 +5,7 @@ class CloneRepo implements Serializable {
     CloneRepo(steps) { this.steps = steps }
 
     def call(String repoUrl, String branchName, String credentialsId) {
-        steps.echo "Cloning repo: ${repoUrl}, branch: ${branchName}"
+        steps.echo "Cloning repo: ${repoUrl}, branch: ${branchName} using credentialsId: ${credentialsId}"
         steps.git branch: branchName, credentialsId: credentialsId, url: repoUrl
     }
 }
