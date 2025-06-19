6232bde982fa3b0b4b65df80b73c30f3b1e0ed8c
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-06-19 08:21:54 +0530

    Update SonarScanner.groovy

--- src/org/common/SonarScanner.groovy
+++ src/org/common/SonarScanner.groovy
@@ -4,16 +4,20 @@ class SonarScanner implements Serializable {
     def steps
     SonarScanner(steps) { this.steps = steps }
 
-    def call(String sonarKey, String sonarUrl, String sonarToken) {
-        steps.echo "Running SonarQube analysis..."
+   def call(String sonarKey, String sonarUrl, String credentialId) {
+    steps.echo "Running SonarQube analysis..."
+    
+    // Use Jenkins credentials to get the actual token securely
+    steps.withCredentials([steps.string(credentialsId: src-sonar, variable: 'SONAR_TOKEN')]) {
         steps.withSonarQubeEnv('sonar') {
             steps.sh """
                 sonar-scanner \
                     -Dsonar.projectKey=${sonarKey} \
                     -Dsonar.sources=. \
                     -Dsonar.host.url=${sonarUrl} \
-                    -Dsonar.login=${sonarToken}
+                    -Dsonar.login=${SONAR_TOKEN}
             """
         }
     }
 }
+
