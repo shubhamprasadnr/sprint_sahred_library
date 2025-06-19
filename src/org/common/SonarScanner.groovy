403e0d478751e98f6e6b903d41b8247995b0f664
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-06-19 07:56:07 +0530

    Update SonarScanner.groovy

--- src/org/common/SonarScanner.groovy
+++ src/org/common/SonarScanner.groovy
@@ -6,7 +6,7 @@ class SonarScanner implements Serializable {
 
     def call(String sonarKey, String sonarUrl, String sonarToken) {
         steps.echo "Running SonarQube analysis..."
-        steps.withSonarQubeEnv('soanr') {
+        steps.withSonarQubeEnv('SonarQube Scanner') {
             steps.sh """
                 sonar-scanner \
                     -Dsonar.projectKey=${sonarKey} \
