617b51ce60cb5245e1a0e911d24cfa29fe3403ac
shubhamprasadnr <163019869+shubhamprasadnr@users.noreply.github.com>
2025-06-20 10:33:02 +0530

    Update SonarScanner.groovy

--- src/org/common/SonarScanner.groovy
+++ src/org/common/SonarScanner.groovy
@@ -7,18 +7,28 @@ class SonarScanner implements Serializable {
         this.steps = steps
     }
 
-    def call(String sonarKey, String sonarUrl, String credentialId) {
+    def call(String sonarKey, String sonarUrl, String credentialId, String buildTool = "maven") {
+        steps.echo "Compiling project..."
+        if (buildTool == "maven") {
+            steps.sh 'mvn clean compile'
+        } else if (buildTool == "gradle") {
+            steps.sh './gradlew build'
+        }
+
         steps.echo "Running SonarQube analysis..."
 
         steps.withSonarQubeEnv('sonar') {
             steps.withCredentials([steps.string(credentialsId: credentialId, variable: 'SONAR_TOKEN')]) {
-                steps.sh '''
+                def binariesDir = (buildTool == "maven") ? "target/classes" : "build/classes"
+
+                steps.sh """
                     sonar-scanner \\
                       -Dsonar.projectKey=${sonarKey} \\
                       -Dsonar.sources=. \\
                       -Dsonar.host.url=${sonarUrl} \\
-                      -Dsonar.login=$SONAR_TOKEN
-                '''
+                      -Dsonar.login=\$SONAR_TOKEN \\
+                      -Dsonar.java.binaries=${binariesDir}
+                """
             }
         }
     }
